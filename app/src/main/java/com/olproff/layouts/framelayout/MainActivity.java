edbccdb3f777eaa05a8f88c4708bb285bc15d474
olproff <olproff@yandex.ru>
2022-04-25 15:27:56 +0300

    Initial commit

---
+++ app/src/main/java/com/olproff/layouts/framelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.olproff.layouts.framelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
